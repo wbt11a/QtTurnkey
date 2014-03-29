a89b061d6771c518d3b354bce68893c20d7a218f
beaty <beaty@korriban.org>
2014-03-29 13:05:43 -0400

    Added some try-catches to parser to prevent flameouts

--- Milestone2/parser.py
+++ Milestone2/parser.py
@@ -9,6 +9,12 @@ def get_links(url):
     next = ""
     next_url = url
     links =[]
+    #hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    #   'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    #   'Accept-Encoding': 'none',
+    #   'Accept-Language': 'en-US,en;q=0.8',
+    #   'Connection': 'keep-alive'}
     while next_url:
         resp = urllib2.urlopen(next_url)
         firstpage = resp.read()
@@ -19,19 +25,26 @@ def get_links(url):
             if classes != None:
                 for c in classes:
                     if c == "title":
-                        r = urllib2.urlopen(top_url+link.get('href'))
+                        try:
+                            r = urllib2.urlopen(top_url+link.get('href'))
+                        except urllib2.HTTPError, err1:
+                            print err1.fp.read()
                         page = r.read()
                         d = BeautifulSoup(page)
                         ovf_string = d.find(text="OVF")
                         if ovf_string:
                             a_tag = ovf_string.find_parent('a')
                             if a_tag:
-                                r2 = urllib2.urlopen(top_url+a_tag.get('href'))
+                                try:
+                                    r2 = urllib2.urlopen(top_url+a_tag.get('href'))
+                                except urllib2.HTTPError, err2:
+                                    print err2.fp.read()
                                 last_page = r2.read()
                                 d_last = BeautifulSoup(last_page)
                                 direct_link_str = d_last.find(text="direct link")
                                 if direct_link_str:
                                    out_file.write(link.get_text().split("-")[0] + " : " +direct_link_str.find_parent('a').get('href'))
+                                   #print link.get_text().split("-")[0] + " : " +direct_link_str.find_parent('a').get('href')
                                    out_file.flush()
             if "next" in link.get_text():
                 next = top_url+link.get('href')
